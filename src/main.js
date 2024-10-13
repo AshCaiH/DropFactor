ec68118121e0cb1baf4f1490a725ae15699c2c19
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-10-13 18:20:04 +0100

    display coin grid

--- src/main.js
+++ src/main.js
@@ -1,33 +1,50 @@
-import { Sprite, init, GameLoop } from "../node_modules/kontra/kontra.mjs";
-
+import { init, GameLoop, SpriteClass } from "../node_modules/kontra/kontra.mjs";
 
 let { canvas } = init();
 
-let sprite = Sprite({
-    x: 100,
-    y: 100,
-    // dx: 2, // Moves each frame
+let sprites = [];
 
-    color: '#DDD',
+let gridSize = {width: 7, height: 7};
+let coinBuffer = 2;
+let coinRadius = 30;
 
-    // custom properties
-    radius: 20,
-
-    render: function () {
-        this.context.fillStyle = this.color;
-
-        this.context.beginPath();
-        this.context.arc(0, 0, this.radius, 0, 2 * Math.PI);
-        this.context.fill();
+class Coin extends SpriteClass {
+    constructor(properties) {
+        properties = Object.assign({
+            color: '#DDD'
+        }, properties);
+        super(properties);
     }
-});
+
+    draw() {
+        this.context.fillStyle = this.color;
+        this.context.beginPath();
+        this.context.arc(this.x + coinRadius, this.y + coinRadius, coinRadius, 0, 2 * Math.PI);
+        this.context.fill();
+        // super.draw();
+    }
+}
+
+for (let x = 0; x < gridSize.width; x++) {
+for (let y = 0; y < gridSize.height; y++) {
+    let sprite = new Coin({
+        x: x * (coinRadius + coinBuffer),
+        y: y * (coinRadius + coinBuffer),
+        color: "#ABC"
+    });    
+
+    sprites.push(sprite);
+}
+}
+
+console.log(sprites[0]);
 
 let loop = GameLoop({  // create the main game loop
     update: function () { // update the game state
-        sprite.update();
+        sprites.forEach((sprite) => sprite.update());
     },
     render: function () { // render the game state
-        sprite.render();
+        sprites.forEach((sprite) => sprite.render());
     }
 });
 

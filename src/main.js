6444d6fe194d91602022c2606ee6550e2bce38ba
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-10-16 13:50:09 +0100

    assign value to coin and colour accordingly

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { init, GameLoop, Sprite, Text } from "../node_modules/kontra/kontra.mjs";
+import { init, GameLoop, Sprite, Text, GameObject, randInt } from "../node_modules/kontra/kontra.mjs";
 
 let { canvas } = init();
 
@@ -7,14 +7,20 @@ let sprites = [];
 let coins = [];
 
 let gridSize = {width: 7, height: 7};
-let coinBuffer = 2;
+let coinBuffer = 4;
 let coinRadius = 20;
+
+let palette = ["#003f5c","#374c80","#7a5195","#bc5090","#ef5675","#ff764a","#ffa600"]
 
 
 function makeCoin(x,y) {
+    let value = randInt(1,7)
+    let isBuried = randInt(1,10) == 10
+
     let text = Text({
-        text: "1",
-        color: "#333",
+        opacity: isBuried ? 0: 1,
+        text: value,
+        color: value <= 3 ? "#CDE" : "#311",
         font: 'bold 24px Arial',
         height: coinRadius * 2,
         width: coinRadius * 2,
@@ -23,23 +29,31 @@ function makeCoin(x,y) {
     });
 
     let background = Sprite({
+        color: isBuried ? "#ABC": palette[value-1],
         render: function() {
-            this.context.fillStyle = "#ABC";
+            this.context.fillStyle = this.color;
+            this.context.lineWidth = 2;
+            this.context.strokeStyle = "#BCD";
+            this.context.stroke();
             this.context.beginPath();
-            this.context.arc(this.x + coinRadius, this.y + coinRadius, coinRadius, 0, 2 * Math.PI);
+            this.context.arc(coinRadius, coinRadius, coinRadius, 0, 2 * Math.PI);
             this.context.fill();
         }
     })
 
     const children = [background, text]
 
-    let sprite = Sprite({
-        x: x * (coinRadius * 2 + coinBuffer),
-        y: y * (coinRadius * 2 + coinBuffer),
+    background
+
+    let coin = GameObject({
+        x: x * (coinRadius * 2 + coinBuffer) + 2,
+        y: y * (coinRadius * 2 + coinBuffer) + 2,
+        bg: background,
+        text: text,
         children
     })
 
-    sprites.push(sprite);
+    sprites.push(coin);
 }
 
 for (let x = 0; x < gridSize.width; x++) {

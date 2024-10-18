9cfaaf6336ef2cfbf31b91ebca502a001d54ade8
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-10-18 13:52:55 +0100

    auto fill with game over

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { init, GameLoop, Sprite, Text, GameObject, randInt } from "../node_modul
 
 let { canvas } = init();
 
-let objects = [];
-
 let gridSize = {width: 7, height: 7};
 let coinBuffer = 12;
 let coinRadius = 30;
@@ -18,90 +16,92 @@ let grid = Array.from({ length:7 }, (item, i) => Array.from({ length:7 }, (item,
 console.log(grid);
 
 function makeCoin(gridX,gridY) {
-    dropping = true;
+	dropping = true;
 
-    let value = randInt(1,7)
-    let isBuried = randInt(1,8) === 8
+	let value = randInt(1,7)
+	let isBuried = randInt(1,8) === 8
 
-    gridY = -1;
+	gridY = -1;
 
-    // How many coins are already in column?
-    let coinsInColumn = grid[gridX].filter(item => item !== null).length;
-    gridY = gridSize.height - coinsInColumn - 1;
-    console.log(gridY);
+	// How many coins are already in column?
+	let coinsInColumn = grid[gridX].filter(item => item !== null).length;
+	gridY = gridSize.height - coinsInColumn - 1;
+	console.log(gridY);
 
-    let text = Text({
-        opacity: isBuried ? 0: 1,
-        text: value,
-        color: value >= 5 ? "#CDE" : "#311",
-        font: 'bold 24px Arial',
-        width: coinRadius * 2,
-        textAlign: "center",
-        anchor: {x: 0, y: -0.8},
-    });
+	if (gridY === -1) gameOver = true;
 
-    let background = Sprite({
-        color: isBuried ? "#ABC": palette[value-1],
-        render: function() {
-            this.context.fillStyle = this.color;
-            this.context.lineWidth = 2;
-            this.context.strokeStyle = "#BCD";
-            this.context.stroke();
-            this.context.beginPath();
-            this.context.arc(coinRadius, coinRadius, coinRadius, 0, 2 * Math.PI);
-            this.context.fill();
-        }
-    })
+	let text = Text({
+		opacity: isBuried ? 0: 1,
+		text: value,
+		color: value >= 5 ? "#CDE" : "#311",
+		font: 'bold 24px Arial',
+		width: coinRadius * 2,
+		textAlign: "center",
+		anchor: {x: 0, y: -0.8},
+	});
 
-    const children = [background, text]
+	let background = Sprite({
+		color: isBuried ? "#ABC": palette[value-1],
+		render: function() {
+			this.context.fillStyle = this.color;
+			this.context.beginPath();
+			this.context.arc(coinRadius, coinRadius, coinRadius, 0, 2 * Math.PI);
+			this.context.fill();
+			this.context.closePath();
+		}
+	})
 
-    let coin = GameObject({
-        gridPos: {x: gridX, y: gridY},
-        x: gridX * (coinRadius * 2 + coinBuffer),
-        y: -1 * (coinRadius * 2 + coinBuffer),
-        dy: 24,
-        bg: background,
-        text: text,
-        children,
-        update: function() {
-            if (this.y >= this.gridPos.y * (coinRadius * 2 + coinBuffer)) {
-                this.y = this.gridPos.y * (coinRadius * 2 + coinBuffer);
-                dropping = false;
-                this.dy = 0;
-            } else {
-                this.y += this.dy;
-            }
-        }
-    })
+	let stroke = Sprite({
+		render: function() {
+			this.context.lineWidth = 2;
+			this.context.strokeStyle = "#BCD";
+			this.context.stroke();
+			this.context.beginPath();
+			this.context.arc(coinRadius, coinRadius, coinRadius, 0, 2 * Math.PI);
+			this.context.closePath();
+		}
+	})
 
-    grid[gridX][gridY] = coin;
+	let coin = GameObject({
+		gridPos: {x: gridX, y: gridY},
+		x: gridX * (coinRadius * 2 + coinBuffer),
+		y: -1 * (coinRadius * 2 + coinBuffer),
+		dy: 48,
+		bg: background,
+		text: text,
+		dropping: true,
+		update: function(dt) {
+			this.advance()
+			if (this.dropping && this.y >= this.gridPos.y * (coinRadius * 2 + coinBuffer)) {
+				this.y = this.gridPos.y * (coinRadius * 2 + coinBuffer);
+				dropping = false;
+				this.dropping = false;
+				this.dy = 0;
+			}
+		}
+	})
 
-    return coin;
+	coin.addChild(background, stroke, text);
+
+	grid[gridX][gridY] = coin;
+
+	return coin;
 }
-
-// objects.push(makeCoin(0,0));
-
-for (let x = 0; x < gridSize.width; x++) {
-for (let y = 0; y < gridSize.height; y++) {
-    // while (!dropping) {
-        objects.push(makeCoin(x,y));
-    // }
-}}
-
-// if (!dropping) {
-//     objects.push(makeCoin(randInt(0,gridSize.width),0));
-// }
-
+		
 let camera = GameObject({
-    x: 700 / 2 - (coinRadius + coinBuffer) * (gridSize.width - 1) + coinBuffer,
-    y: coinRadius + coinBuffer * 2,
-    children: objects,
+	x: 700 / 2 - (coinRadius + coinBuffer) * (gridSize.width - 1) + coinBuffer,
+	y: coinRadius + coinBuffer * 2,
 })
 
+function update() {
+	camera.update()
+	if (!dropping && !gameOver)
+		camera.addChild(makeCoin(randInt(0,gridSize.width-1),0));
+}
 
 let loop = GameLoop({
-    update: () => camera.update(),
-    render: () => camera.render()
+	update: (dt) => update(dt),
+	render: () => camera.render(),
 });
 
 loop.start();

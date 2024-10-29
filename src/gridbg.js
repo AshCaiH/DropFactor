7bfdf571f03a6e914be377ee581ec0e9d644a196
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-10-29 23:52:29 +0000

    spun gridcells into own object

--- src/gridbg.js
+++ src/gridbg.js
@@ -1,25 +1,38 @@
-import { SpriteClass } from "../node_modules/kontra/kontra.mjs";
+import { GameObject, SpriteClass } from "../node_modules/kontra/kontra.mjs";
 import { settings } from "./Global.js";
 
+const {slots, coinRadius, coinBuffer} = settings;
+
 export class GridBG extends SpriteClass {
-    constructor() {
-        super({
-        render: function() {
-            const {slots, coinRadius, coinBuffer} = settings;
-            const ctx = this.context;
-            for (let i=0; i<slots.x; i++) {
-            for (let j=0; j<slots.y; j++) {
-                ctx.lineWidth = 1.5;
-                ctx.strokeStyle = "#345";
-                ctx.beginPath();
-                ctx.strokeRect(
-                    i*(coinRadius * 2 + coinBuffer)-coinBuffer/2,
-                    j*(coinRadius * 2 + coinBuffer)-coinBuffer/2,
-                    coinRadius * 2 + coinBuffer,
-                    (coinRadius * 2 + coinBuffer)
-                );
-                ctx.closePath();
-            }}
-        }});
-    }
+	constructor() {
+		super();
+		for (let i=0; i<slots.x; i++) {
+		for (let j=0; j<slots.y; j++) {
+			this.addChild(new GridBGCell({x: i, y:j}));
+		}}
+	}
+}
+
+class GridBGCell extends GameObject {
+	constructor(gridPosition) {
+		super({
+			gridPosition: gridPosition,
+			render: () => {
+				const ctx = this.context;
+				ctx.lineWidth = 1.5;
+				ctx.strokeStyle = "#345";
+				ctx.beginPath();
+				ctx.strokeRect(
+					gridPosition.x*(coinRadius * 2 + coinBuffer)-coinBuffer/2,
+					gridPosition.y*(coinRadius * 2 + coinBuffer)-coinBuffer/2,
+					coinRadius * 2 + coinBuffer,
+					(coinRadius * 2 + coinBuffer)
+				);
+				ctx.closePath();
+			},
+			update: () => {
+
+			},
+		});
+	}
 }
